9319a5e9a5b10858d80a1b4df3a48a9a3936c840
Madyo <y.lebars@numericite.eu>
2023-06-29 12:12:59 +0200

    feat: add links stores

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse, userAgent } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+
+  const { device } = userAgent(request)
+  const viewport = device.type === 'mobile' ? 'mobile' : 'desktop'
+  const OS = device.model === 'iPhone' ? 'iOS' : 'Android'
+
+  if(viewport === 'mobile' && OS === 'iOS') {
+    return NextResponse.redirect('https://apps.apple.com/us/app/nata/id1670657282')
+  }
+  if(viewport === 'mobile' && OS === 'Android') {
+    return NextResponse.redirect('https://play.google.com/store/apps/details?id=com.nata01&gl=FR&pli=1')
+  }
+
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/app-stores',
+}
